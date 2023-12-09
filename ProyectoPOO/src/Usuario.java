b5ee1ca661e17dafb429c78773938e426da4f7c4
Samantha Mojica <112099657+sammojica@users.noreply.github.com>
2023-12-09 12:58:54 -0600

    Update Usuario.java

--- ProyectoPOO/src/Usuario.java
+++ ProyectoPOO/src/Usuario.java
@@ -1,221 +1,249 @@
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Carrito {
+public class Usuario extends Persona {
 
-    private Map<Producto, Integer> productosEnCarrito;
+    public int codigoPostal;
+    public String nombreUsuario;
+    public double puntos;
+    public int nivel;
+    public String Sucursal;
 
-    public Carrito() {
-        this.productosEnCarrito = new HashMap<>();
+    public Carrito carrito;
+    public List<Compra> historialCompras;
+
+    public Usuario(int codigoPostal, String nombreUsuario, double puntos, int nivel, String Sucursal, Carrito carrito, List<Compra> historialCompras, String nombre, String apellidos, String direccion, String telefono, String correoElectronico, String contraseña) {
+        super(nombre, apellidos, direccion, telefono, correoElectronico, contraseña);
+        this.codigoPostal = codigoPostal;
+        this.nombreUsuario = nombreUsuario;
+        this.puntos = puntos;
+        this.nivel = nivel;
+        this.Sucursal = Sucursal;
+        // Asignar carrito solo si no es null, de lo contrario, crea un nuevo Carrito
+        this.carrito = (carrito != null) ? carrito : new Carrito();
+
+        // Asignar historialCompras solo si no es null, de lo contrario, crea una nueva lista
+        this.historialCompras = (historialCompras != null) ? historialCompras : new ArrayList<>();
     }
 
-    public Map<Producto, Integer> getProductosEnCarrito() {
-        return productosEnCarrito;
+    public String toString() {
+        return String.format(
+                "%d,%s,%f,%d,%s,%s,%s,%s,%s,%s,%s,%s,%s",
+                codigoPostal, nombreUsuario, puntos, nivel, Sucursal,
+                carritoToString(), historialComprasToString(), nombre, apellidos,
+                direccion, telefono, correoElectronico, contraseña);
     }
 
-    public void agregarProducto(Producto producto, int cantidad, String sucursal) {
-        if (cantidad <= 0) {
-            System.out.println("La cantidad debe ser mayor que cero. No se ha agregado el producto al carrito.");
-            return;
-        }
-        // Obtener el stock disponible en la sucursal correspondiente
-        int stockDisponible = obtenerStockDisponible(producto, sucursal);
-        if (stockDisponible < cantidad) {
-            System.out.println("No hay suficiente stock disponible. No se ha agregado el producto al carrito.");
-            return;
-        }
-        // Actualizar el carrito con el nuevo producto y cantidad
-        productosEnCarrito.put(producto, productosEnCarrito.getOrDefault(producto, 0) + cantidad);
+    public static Usuario fromString(String linea) {
+        String[] partes = linea.split(",");
+        int codigoPostal = Integer.parseInt(partes[0]);
+        String nombreUsuario = partes[1];
+        double puntos = Double.parseDouble(partes[2]);
+        int nivel = Integer.parseInt(partes[3]);
+        String Sucursal = partes[4];
+        Carrito carrito = parseCarrito(partes[5]);  // Implementa parseCarrito según tus necesidades
+        List<Compra> historialCompras = parseHistorial(partes[6]);  // Implementa parseHistorial
+        String nombre = partes[7];
+        String apellidos = partes[8];
+        String direccion = partes[9];
+        String telefono = partes[10];
+        String correoElectronico = partes[11];
+        String contraseña = partes[12];
+
+        return new Usuario(codigoPostal, nombreUsuario, puntos, nivel, Sucursal, carrito, historialCompras, nombre, apellidos, direccion, telefono, correoElectronico, contraseña);
     }
 
-    public void editarUnidadesProducto(Producto producto, int nuevaCantidad) {
-        if (productosEnCarrito.containsKey(producto)) {
-            productosEnCarrito.put(producto, nuevaCantidad);
-            System.out.println("Cantidad actualizada correctamente.");
-        } else {
-            System.out.println("El producto no está en el carrito.");
-        }
+    private String carritoToString() {
+        // Implementa la lógica para convertir el carrito a una cadena
+        return (carrito != null) ? carrito.toString() : "null";
     }
 
-// Método para eliminar un producto del carrito
-    public void eliminarProducto(Producto producto) {
-        if (productosEnCarrito.containsKey(producto)) {
-            productosEnCarrito.remove(producto);
-            System.out.println("Producto eliminado del carrito.");
-        } else {
-            System.out.println("El producto no está en el carrito.");
-        }
+    private String historialComprasToString() {
+        // Implementa la lógica para convertir el historial de compras a una cadena
+        return (historialCompras != null) ? historialCompras.toString() : "null";
     }
 
-    private int obtenerStockDisponible(Producto producto, String sucursal) {
-        // Devuelve el stock disponible correspondiente a la sucursal
-        int stockDisponible = 0;  // Puedes ajustar este valor de retorno según lo que tenga sentido en tu aplicación
-        switch (sucursal.toLowerCase()) {
-            case "norte":
-                stockDisponible = producto.getStockNorte();
-                break;
-            case "sur":
-                stockDisponible = producto.getStockSur();
-                break;
-            case "centro":
-                stockDisponible = producto.getStockCentro();
-                break;
-            default:
-                System.out.println("Sucursal no válida.");
-                break;
-        }
-        return stockDisponible;
+    private static Carrito parseCarrito(String cadenaCarrito) {
+        // Implementa la lógica para convertir la cadena a un objeto Carrito
+        return (cadenaCarrito.equals("null")) ? null : new Carrito();  // Ajusta según tu lógica real
     }
 
-    public static void verCarrito(Usuario usuario, String sucursal) {
-        Carrito carrito = usuario.getCarrito();
-        if (carrito.getProductosEnCarrito().isEmpty()) {
-            System.out.println("El carrito está vacío.");
-            return;
+    private static List<Compra> parseHistorial(String cadenaHistorial) {
+        // Implementa la lógica para convertir la cadena a una lista de Compra
+        return (cadenaHistorial.equals("null")) ? new ArrayList<>() : new ArrayList<>();  // Ajusta según tu lógica real
+    }
+
+    public int getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(int codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public double getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(double puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getSucursal() {
+        return Sucursal;
+    }
+
+    public void setSucursal(String Sucursal) {
+        this.Sucursal = Sucursal;
+    }
+
+    public Carrito getCarrito() {
+        return carrito;
+    }
+
+    public void setCarrito(Carrito carrito) {
+        this.carrito = carrito;
+    }
+
+    public List<Compra> getHistorialCompras() {
+        return historialCompras;
+    }
+
+    public void agregarCompraAlHistorial(Compra compra) {
+        if (historialCompras == null) {
+            historialCompras = new ArrayList<>();
         }
+        historialCompras.add(compra);
+    }
+
+    public static void main() {
         Scanner scan = new Scanner(System.in);
-        boolean salir = false;
-        do {
-            System.out.println("**Contenido del Carrito**");
-            for (Map.Entry<Producto, Integer> entry : carrito.getProductosEnCarrito().entrySet()) {
-                Producto producto = entry.getKey();
-                int cantidad = entry.getValue();
-                System.out.println("Nombre: " + producto.getNombre() + ", Cantidad: " + cantidad);
+
+        System.out.println("**Inicio de sesión**");
+        List<Usuario> usuariosRegistrados = RegistroClientes.leerClientesDesdeArchivo();
+        // Verificar si hay usuarios registrados
+        if (usuariosRegistrados.isEmpty()) {
+            System.out.println("Aún no hay usuarios registrados. Regístrese primero.");
+            return;  // Salir del programa si no hay usuarios registrados
+        }
+
+        boolean inicioSesionExitoso = false;
+
+        while (!inicioSesionExitoso) {
+            System.out.println("Ingrese su nombre de usuario (o 's' para salir):");
+            String nombreUsuarioIngresado = scan.nextLine();
+
+            if (nombreUsuarioIngresado.equalsIgnoreCase("s")) {
+                System.out.println("Saliendo del inicio de sesión...");
+                return; // Salir del programa o realizar alguna acción de salida
             }
-            System.out.println("\n¿Qué acción desea realizar?");
-            System.out.println("1. Comprar los productos en el carrito");
-            System.out.println("2. Editar unidades de un producto");
-            System.out.println("3. Eliminar un producto del carrito");
-            System.out.println("0. Salir");
 
-            int opcion = scan.nextInt();
-            scan.nextLine(); // Consumir la nueva línea pendiente después del nextInt
+            System.out.println("Ingrese su contraseña:");
+            String contraseñaIngresada = scan.nextLine();
 
-            switch (opcion) {
-                case 1:
-                    System.out.println("¿Desea comprar los productos en el carrito? (s/n)");
-                    String respuestaCompra = scan.nextLine().toLowerCase();
-                    if (respuestaCompra.equals("s")) {
-                        // Realizar la compra y actualizar el stock
-                        realizarCompra(carrito, usuario, sucursal);
-                        salir = true;
-                    } else {
-                        System.out.println("Compra cancelada.");
-                    }
-                    break;
-                case 2:
-                    if (carrito.getProductosEnCarrito().isEmpty()) {
-                        System.out.println("El carrito está vacío. No hay productos para editar.");
-                    } else {
-                        System.out.println("Ingrese el nombre del producto que desea editar:");
-                        String nombreProductoEditar = scan.nextLine();
-                        Producto productoEditar = obtenerProductoEnCarritoPorNombre(carrito, nombreProductoEditar);
-                        if (productoEditar != null) {
-                            System.out.println("Ingrese la nueva cantidad:");
-                            int nuevaCantidad = scan.nextInt();
-                            carrito.editarUnidadesProducto(productoEditar, nuevaCantidad);
-                        } else {
-                            System.out.println("Producto no encontrado en el carrito.");
-                        }
-                    }
-                    break;
-                case 3:
-                    if (carrito.getProductosEnCarrito().isEmpty()) {
-                        System.out.println("El carrito está vacío. No hay productos para eliminar.");
-                    } else {
-                        System.out.println("Ingrese el nombre del producto que desea eliminar:");
-                        String nombreProductoEliminar = scan.nextLine();
-                        Producto productoEliminar = obtenerProductoEnCarritoPorNombre(carrito, nombreProductoEliminar);
-                        if (productoEliminar != null) {
-                            carrito.eliminarProducto(productoEliminar);
-                        } else {
-                            System.out.println("Producto no encontrado en el carrito.");
-                        }
-                    }
-                    break;
-                case 0:
-                    System.out.println("Saliendo del menú.");
-                    salir = true;
-                    break;
-                default:
-                    System.out.println("Opción no válida.");
-                    break;
-            }
-        } while (!salir);
-    }
-
-    private static Producto obtenerProductoEnCarritoPorNombre(Carrito carrito, String nombreProducto) {
-        for (Map.Entry<Producto, Integer> entry : carrito.getProductosEnCarrito().entrySet()) {
-            Producto producto = entry.getKey();
-            if (producto.getNombre().equalsIgnoreCase(nombreProducto)) {
-                return producto;
+            // Validar el nombre de usuario y la contraseña
+            Usuario usuariotemp = validarUsuarioYContraseña(nombreUsuarioIngresado, contraseñaIngresada, usuariosRegistrados);
+            if (usuariotemp != null) {
+                System.out.println("Inicio de sesión exitoso.");
+                inicioSesionExitoso = true;
+                menu(usuariotemp);
+            } else {
+                System.out.println("Nombre de usuario o contraseña incorrectos. Intente de nuevo.");
             }
         }
+    }
+
+    public static Usuario validarUsuarioYContraseña(String nombreUsuario, String contraseña, List<Usuario> usuariosRegistrados) {
+        for (Usuario usuario : usuariosRegistrados) {
+            if (usuario.getNombreUsuario().equals(nombreUsuario) && usuario.getContraseña().equals(contraseña)) {
+                // Validación exitosa, devuelve el objeto Usuario encontrado
+                return usuario;
+            }
+        }
+        // Si no se encuentra un usuario con la combinación correcta de nombre de usuario y contraseña
         return null;
     }
+    
+public static void imprimirHistorialCompras(Usuario usuario) {
+    List<Compra> historialCompras = usuario.getHistorialCompras();
 
-    public static void realizarCompra(Carrito carrito, Usuario usuario, String sucursal) {
-        List<Producto> listaProductos = Producto.leerProductosDesdeArchivo();
-        List<Usuario> listaUsuarios = new ArrayList<>(RegistroClientes.leerClientesDesdeArchivo());
+    if (historialCompras == null || historialCompras.isEmpty()) {
+        System.out.println("El historial de compras está vacío.");
+    } else {
+        System.out.println("Historial de compras para el usuario " + usuario.getNombre() + ":");
+        for (Compra compra : historialCompras) {
+            System.out.println("Fecha de compra: " + compra.getFechaCompra());
+            System.out.println("Productos comprados:");
 
-        // Verificar el stock antes de realizar la compra
-        for (Map.Entry<Producto, Integer> entry : carrito.getProductosEnCarrito().entrySet()) {
-            Producto productoEnCarrito = entry.getKey();
-            int cantidadEnCarrito = entry.getValue();
-            int stockDisponible = carrito.obtenerStockDisponible(productoEnCarrito, sucursal);
-
-            if (cantidadEnCarrito > stockDisponible) {
-                System.out.println("No hay suficiente stock para el producto '" + productoEnCarrito.getNombre() + "'.");
-                System.out.println("Stock disponible: " + stockDisponible);
-                System.out.println("Compra cancelada.");
-                return;
+            for (Map.Entry<Producto, Integer> entry : compra.getProductosComprados().entrySet()) {
+                Producto producto = entry.getKey();
+                int cantidad = entry.getValue();
+                System.out.println("- " + producto.getNombre() + ": " + cantidad);
             }
+
+            System.out.println("------------------------------");
         }
+    }
+}
 
-        // Actualizar el stock y vaciar el carrito
-        for (Map.Entry<Producto, Integer> entry : carrito.getProductosEnCarrito().entrySet()) {
-            Producto productoEnCarrito = entry.getKey();
-            int cantidadEnCarrito = entry.getValue();
 
-            // Encontrar el producto correspondiente en la lista original
-            Producto productoOriginal = listaProductos.stream()
-                    .filter(p -> p.getNombre().equalsIgnoreCase(productoEnCarrito.getNombre()))
-                    .findFirst()
-                    .orElse(null);
+    public static void menu(Usuario usuario) {
+        Scanner scan = new Scanner(System.in);
+        int op;
 
-            if (productoOriginal != null) {
-                // Actualizar el stock según la sucursal
-                switch (sucursal.toLowerCase()) {
-                    case "norte":
-                        productoOriginal.setStockNorte(productoOriginal.getStockNorte() - cantidadEnCarrito);
-                        break;
-                    case "sur":
-                        productoOriginal.setStockSur(productoOriginal.getStockSur() - cantidadEnCarrito);
-                        break;
-                    case "centro":
-                        productoOriginal.setStockCentro(productoOriginal.getStockCentro() - cantidadEnCarrito);
-                        break;
-                }
+        do {
+            System.out.println("\n\nBienvenido a la SUPER Tienda FI");
+            System.out.println("1. Buscar producto.");
+            System.out.println("2. Ver carrito.");
+            System.out.println("3. Actualizar datos personales.");
+            System.out.println("4. Ver mis compras.");
+            System.out.println("5. Ver mis puntos.");
+            System.out.println("6. Cerrar sesión.");
+            op = scan.nextInt();
+            switch (op) {
+                case 1:
+                    Producto.buscarProducto(usuario.getCarrito(), usuario.getSucursal());
+                    break;
+                case 2:
+                    Carrito.verCarrito(usuario, usuario.getSucursal());
+                    break;
+                case 3:
+                    RegistroClientes.actualizarDatosPersonales(usuario.getNombreUsuario());
+                    break;
+                case 4:
+                    imprimirHistorialCompras(usuario);
+                    break;
+                case 5:
+                    double puntos = usuario.getPuntos();
+                    System.out.println("Total de puntos: " + puntos);
+                    break;
+                case 6:
+                    System.out.println("Cerrando sesión...\n\n");
+                    break;
+                default:
+                    System.out.println("Opción inválida.");
+                    break;
             }
-        }
-Compra nuevaCompra = new Compra(carrito.getProductosEnCarrito());
-    usuario.agregarCompraAlHistorial(nuevaCompra);
-
-    // Actualizar la lista de usuarios con el usuario modificado
-    listaUsuarios.removeIf(u -> u.getNombre().equalsIgnoreCase(usuario.getNombre()));
-    listaUsuarios.add(usuario);
-
-    // Guardar los cambios en el archivo
-    RegistroClientes.guardarClientesEnArchivo(listaUsuarios);
-    Producto.guardarProductosEnArchivo(listaProductos);
-
-    // Vaciar el carrito
-    carrito.getProductosEnCarrito().clear();
-
-    System.out.println("Compra realizada con éxito. ¡Gracias por su compra!");
+        } while (op != 6);
     }
 
 }
