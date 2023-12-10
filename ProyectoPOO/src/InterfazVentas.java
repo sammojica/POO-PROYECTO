91f7f6754052b44bd2582bb729bb0e527faca399
Samantha Mojica <112099657+sammojica@users.noreply.github.com>
2023-12-10 14:09:20 -0600

    Update InterfazVentas.java
    VERSION FINAL CON ACERCA DE...

--- ProyectoPOO/src/InterfazVentas.java
+++ ProyectoPOO/src/InterfazVentas.java
@@ -1,42 +1,43 @@
 //Clase principal
-import java.util.InputMismatchException;
 import java.util.Scanner;
 public class InterfazVentas{
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner scan = new Scanner(System.in);
-        int op=0;
+        System.out.println("**PROYECTO FINAL**");
+        System.out.println("-Programación Orientada a Objetos");
+        System.out.println("-Grupo 7");
+        System.out.println("-Semestre 2024-1");
+        System.out.println("-Equipo Bee-T");
+        System.out.println("-Integrantes:\nHernandez Sanchez Karla. \nMeneses Calderas Grecia Irais. \nMojica Pereda Elena Samantha. \nRuiz Cervantes Karla Patricia.\n\n");
+        int op;
+        Thread.sleep(3000);
         do{
-            try{
-                System.out.println("\n\tSistema de gestión");
-                System.out.println("----------- SUPER Tienda FI -----------");
-                System.out.println("\nEscoja su tipo de sesión:");
-                System.out.println("1. Cliente.");
-                System.out.println("2. Empleado.");
-                System.out.println("3. Gerente");
-                System.out.print("4. Salir\n  > ");
-                op = scan.nextInt();
-
-                switch(op){
-                    case 1:
-                        Cliente.main();
-                        break;
-                    case 2:
-                        Empleado.main();
-                        break;
-                    case 3:
-                        Gerente.main();
-                        break;
-                    case 4:
-                        System.out.println("Saliendo del programa, hasta luego.");
-                        break;
-                    default:
-                        break;
-                }
-            }catch (InputMismatchException e) {
-                System.out.println("\nOpción inválida. Intente de nuevo.");
-                scan.nextLine(); 
-            } 
+        System.out.println("Sistema de gestión");
+        System.out.println("-----------SUPER Tienda FI-----------");
+        System.out.println("\nEscoja su tipo de sesión:");
+        System.out.println("1. Cliente.");
+        System.out.println("2. Empleado.");
+        System.out.println("3. Gerente");
+        System.out.println("4. Salir");
+        op = scan.nextInt();
+       
+        switch(op){
+            case 1:
+                Cliente.main();
+                break;
+            case 2:
+                Empleado.main();
+                break;
+            case 3:
+                Gerente.main();
+                break;
+            case 4:
+                System.out.println("Saliendo del programa, hasta luego.");
+                break;
+            default:
+                break;
+        }
         }while(op!=4);
         
     }
