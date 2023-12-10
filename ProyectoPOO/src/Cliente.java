2fdd6428fae7317ab69052195b44bde427bc9c4e
Samantha Mojica <112099657+sammojica@users.noreply.github.com>
2023-12-09 18:06:18 -0600

    Update Cliente.java

--- ProyectoPOO/src/Cliente.java
+++ ProyectoPOO/src/Cliente.java
@@ -1,47 +1,50 @@
+
 import java.util.Scanner;
 
 public class Cliente {
+
     public static void main() {
-    Scanner scan = new Scanner(System.in);
-    int op;
-    pregunta();
-    Carrito carritoTemporal = new Carrito(); 
-    System.out.println("\n\nSistema de compra sin inicio de sesión.\nBienvenido a la SUPER Tienda FI");
-    System.out.println("¿En qué sucursal realizará la compra? [o presione 's' para salir]");
-    String sucursaltemp = scan.nextLine();
-    
-    while (!sucursaltemp.equalsIgnoreCase("s")) {
+        Scanner scan = new Scanner(System.in);
+        int op;
+        pregunta();
+        Carrito carritoTemporal = new Carrito();
+        System.out.println("\n\nSistema de compra sin inicio de sesión.\nBienvenido a la SUPER Tienda FI");
+
+        String sucursaltemp;
         do {
+            System.out.println("¿En qué sucursal realizará la compra? [norte, sur, centro, o presione 's' para salir]");
+            sucursaltemp = scan.nextLine().toLowerCase();
+
+            if (sucursaltemp.equals("s")) {
+                System.out.println("Saliendo del programa...\n\n");
+                return;
+            } else if (!sucursaltemp.equals("norte") && !sucursaltemp.equals("sur") && !sucursaltemp.equals("centro")) {
+                System.out.println("Sucursal inválida. Por favor, ingrese una sucursal válida.");
+            }
+        } while (!sucursaltemp.equals("norte") && !sucursaltemp.equals("sur") && !sucursaltemp.equals("centro"));
+
+        while (true) {
             System.out.println("1. Buscar producto.");
             System.out.println("2. Ver carrito.");
             System.out.println("3. Salir");
             op = scan.nextInt();
-            switch(op) {
+            switch (op) {
                 case 1:
                     Producto.buscarProducto(carritoTemporal, sucursaltemp);
                     break;
                 case 2:
-                    Carrito.verCarritoSinUsuario(carritoTemporal, sucursaltemp);
+                    //Carrito.verCarritoSinUsuario(carritoTemporal, sucursaltemp);
                     break;
                 case 3:
                     System.out.println("Saliendo del menú...\n\n");
-                    break;
+                    return;
                 default:
                     System.out.println("Opción inválida.");
                     break;
             }
-        } while (op != 3);
-
-        System.out.println("¿En qué sucursal realizará la compra? [o presione 's' para salir]");
-        scan.nextLine(); // Consumir la nueva línea pendiente después de nextInt
-        sucursaltemp = scan.nextLine();
+        }
     }
 
-    System.out.println("Saliendo del programa...\n\n");
-}
-
-    
-    
     public static void pregunta() {
         Scanner scan = new Scanner(System.in);
         boolean salir = false;
@@ -74,7 +77,7 @@ public class Cliente {
                         salir = true;
                     } else if (respuesta3.equalsIgnoreCase("n")) {
                         System.out.println("Entendido... iniciando registro.");
-                        RegistroClientes.main(); 
+                        RegistroClientes.main();
                         salir = true;
                     } else {
                         System.out.println("Respuesta inválida");
